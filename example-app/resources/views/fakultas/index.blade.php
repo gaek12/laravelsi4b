6fbc792d061ca67b263ed43c008337070e58885a
gaek12 <160828231+gaek12@users.noreply.github.com>
2024-05-21 17:01:19 +0700

    update

--- example-app/resources/views/fakultas/index.blade.php
+++ example-app/resources/views/fakultas/index.blade.php
@@ -1,18 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>UMDP</h1>
+@extends('layout.main')
+
+@section('title','fakultas')
+
+@section('content')
+<h1>UMDP</h1>
     <h2>Fakultas</h2>
+
     <ul>
         @foreach ($fakultas as $item)
-             <li>{{ $item }}</li>
-        @endforeach
+            <li>{{ $item['nama'] }} {{ $item['singkatan'] }}</li>
+        @endsection
     </ul>
-</body>
-</html>
+    <div class="col-lg-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Fakultas</h4>
+                  <p class="card-description">
+                    List data fakultas
+                    Add class <code>.table</code>
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Nama Fakultas</th>
+                          <th>Singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($fakultas as $item)
+                            <tr>
+                            <td>{{ $item['nama'] }}</td>
+                            <td>{{ $item['singkatan'] }}</td>
+                        @endsection
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+@endsection
+
+@endsection
